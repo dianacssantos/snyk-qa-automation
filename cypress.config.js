a9a2ded7c814742c4cbccd2d4b90c6efb96e831f
dianasantos <diana.cs.santos@gmail.com>
2025-11-08 22:33:31 +0000

    fix: add support file

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://cova-dev.vulnapp.io:8080',
-    specPattern: "cypress/e2e/**/*.cy.js",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     fixturesFolder: 'cypress/fixtures',
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
